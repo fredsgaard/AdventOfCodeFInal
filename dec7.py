767fba0d8ebb195db3f7310a80c6f242bd78ee94
anders <anders@fredsgaard.eu>
2022-12-07 14:13:46 +0100

    dec. 7 not done

--- dec7.py
+++ dec7.py
@@ -5,27 +5,49 @@ import re
 def dec7_part1():
     regex_filesize = re.compile(r'(^\d+)')
     regex_ls = re.compile(r'(^(\$\sls))')
-    regex_cd = re.compile(r'(^\$\scd\s\.+)')
+    regex_cd_up = re.compile(r'(^\$\scd\s\.+)')
+    regex_cd_in = re.compile(r'^\$\scd\s([a-zA-Z]+)')
     with open('dec7_test.csv') as csv_file:
         csv_reader = csv.reader(csv_file)
         total_dir_size = 0
         dir_size = 0
+        dir_name = 'root'
+        exec(f'{dir_name} = []')
+        line = 1
         for row in csv_reader:
+            print('line: ' + str(line))
             ls_command = re.fullmatch(regex_ls, row[0])
-            cd_command = re.fullmatch(regex_cd, row[0])
+            cd_up = re.fullmatch(regex_cd_up, row[0])
+            cd_in = re.search(regex_cd_in, row[0])
             file = re.search(regex_filesize, row[0])
+            if cd_in:
+                dir = cd_in.group(1)
+                exec(f'dir_name = "dir_{dir}"')
+                exec(f'{dir_name} = []')
+                print('dir name: ' + dir_name)
             if ls_command:
                 print('$ ls')
                 if dir_size <= 100000:
                     total_dir_size += dir_size
                     print('Total dir size:' + str(total_dir_size))
-            if cd_command:
-                dir_size = 0
-            if file:
+                    dir_size = 0
+            if file:  # Read file size
                 file_size = int(file.group(1))
-                print('File size: ' + str(file_size))
                 dir_size += file_size
-                print('Dir size: ' + str(dir_size))
+                print('File size: ' + str(file_size) + ', ' + 'Dir size: ' + str(dir_size))
+                exec(f'{dir_name}.append(file_size)')
+            if cd_up:  # Move up
+                dir_size = 0
+                dir = ''
+                continue
+            if cd_in:
+                dir = cd_in.group(1)
+                exec(f'dir_name = []')
+            if file:  # Read file size
+                file_size = int(file.group(1))
+                dir_size += file_size
+                print('File size: ' + str(file_size) + ', ' + 'Dir size: ' + str(dir_size))
+            line += 1
         print('Total dir sizes of dirs < 100000: ' + str(total_dir_size))
 
 #
