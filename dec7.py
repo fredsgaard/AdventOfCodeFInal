cbf378127fcc05a6c2b9cff2b421d1030fe97586
Anders Fredsgaard Nielsen <anders@fredsgaard.eu>
2022-12-08 08:22:57 +0100

    dec 7 med dicts

--- dec7.py
+++ dec7.py
@@ -14,6 +14,10 @@ def dec7_part1():
         dir_name = 'root'
         exec(f'{dir_name} = []')
         line = 1
+        dir_level = 0
+        filesystem = {dir_name: []}
+        dir_path = 'root'
+        dir_path_stack = ['root']
         for row in csv_reader:
             print('line: ' + str(line))
             ls_command = re.fullmatch(regex_ls, row[0])
@@ -21,32 +25,38 @@ def dec7_part1():
             cd_in = re.search(regex_cd_in, row[0])
             file = re.search(regex_filesize, row[0])
             if cd_in:
-                dir = cd_in.group(1)
-                exec(f'dir_name = "dir_{dir}"')
-                exec(f'{dir_name} = []')
-                print('dir name: ' + dir_name)
-            if ls_command:
-                print('$ ls')
-                if dir_size <= 100000:
-                    total_dir_size += dir_size
-                    print('Total dir size:' + str(total_dir_size))
-                    dir_size = 0
-            if file:  # Read file size
-                file_size = int(file.group(1))
-                dir_size += file_size
-                print('File size: ' + str(file_size) + ', ' + 'Dir size: ' + str(dir_size))
-                exec(f'{dir_name}.append(file_size)')
+                dir_name = cd_in.group(1)
+                dir_path = dir_path_stack[-1] + '_' + dir_name
+                # dir_level += 1  # Go down 1 level
+                # for lvl in range(dir_level, 0, -1):
+                #     prefix += dir_path_stack[lvl] + '_'
+                # dir_path = prefix + cd_in.group(1)
+                filesystem[dir_path_stack[-1]] = {dir_name: []}
+                dir_path_stack.append(dir_path)
             if cd_up:  # Move up
-                dir_size = 0
-                dir = ''
-                continue
-            if cd_in:
-                dir = cd_in.group(1)
-                exec(f'dir_name = []')
+                dir_path_stack.pop()
             if file:  # Read file size
                 file_size = int(file.group(1))
+                filesystem[dir_path][dir_name].append(int(file.group(1)))
                 dir_size += file_size
                 print('File size: ' + str(file_size) + ', ' + 'Dir size: ' + str(dir_size))
+                # dir_level -= 1
+            # if ls_command:
+            #     print('$ ls')
+            #     if dir_size <= 100000:
+            #         total_dir_size += dir_size
+            #         print('Total dir size:' + str(total_dir_size))
+            #         dir_size = 0
+
+                # exec(f'{dir_name}.append(file_size)')
+
+            # if cd_in:
+            #     dir = cd_in.group(1)
+            #     exec(f'dir_name = []')
+            # if file:  # Read file size
+            #     file_size = int(file.group(1))
+            #     dir_size += file_size
+            #     print('File size: ' + str(file_size) + ', ' + 'Dir size: ' + str(dir_size))
             line += 1
         print('Total dir sizes of dirs < 100000: ' + str(total_dir_size))
 
